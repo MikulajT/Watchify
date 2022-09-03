eb5aeb8570c33480ce6191259a8ea2a0c6eae30b
Tomáš Mikulaj <mikulajt@gmail.com>
2022-09-03 22:16:45 +0200

    first commit

---
+++ BLL/Notifier.cs
@@ -0,0 +1,13 @@
+﻿namespace BLL
+{
+    public static class Notifier
+    {
+        /// <summary>
+        /// Notify all users about upcoming tv shows or movies
+        /// </summary>
+        public static void NotifyAllUsers()
+        {
+
+        }
+    }
+}
---
+++ Watchify/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Watchify.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
