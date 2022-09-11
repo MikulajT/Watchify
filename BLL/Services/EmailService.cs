bea1cbbc3f6a3f90deb8ded7a7657badac4a82d4
Tomáš Mikulaj <mikulajt@gmail.com>
2022-09-11 17:38:38 +0200

    Add email service

---
+++ BLL/Services/EmailService.cs
@@ -0,0 +1,38 @@
+﻿using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+
+namespace BLL.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _config;
+        public EmailService(IConfiguration config) 
+        {
+            _config = config;
+        }
+        public void Send(string from, string to, string subject, string html)
+        {
+            // create message
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(from));
+            email.To.Add(MailboxAddress.Parse(to));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = html };
+
+            // send email
+            using var smtp = new SmtpClient();
+            smtp.Connect(_config["Email:SmtpServer"], int.Parse(_config["Email:Port"]), SecureSocketOptions.StartTls);
+            smtp.Authenticate(_config["Email:Username"], _config["Email:Password"]);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
--- Watchify/Program.cs
+++ Watchify/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddHangfire(x =>
 });
 builder.Services.AddHangfireServer();
 builder.Services.AddTransient<IUsersService, UsersService>();
+builder.Services.AddTransient<IEmailService, EmailService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
